e7b2d11c8d0a85f30560404612323a283c0f8e36
DetectiveMills <sfly9512@gmail.com>
2024-09-10 15:43:35 +0600

    Lesson - 2

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def about(request):
+    return HttpResponse("Hello world")
+
+def index(request):
+    return render(request, 'index.html')
+
+
--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.base.views import index 
+from apps.base.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/,', about)
 ]
